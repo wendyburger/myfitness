eb86ce3c6c6ce214ed11a1a047589743186adc66
Wendy Yang <wendy.y.w.yang@gmail.com>
2016-04-30 15:59:58 +0800

    Added categories to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = ['胸肌','背肌', '肩膀', '手臂', '股四頭肌和小腿肌', '臀肌和腿後肌', '核心肌', '全身訓練', '有氧運動']
+
+categories.each do |muscle|
+  Category.create(name: muscle)
+end
